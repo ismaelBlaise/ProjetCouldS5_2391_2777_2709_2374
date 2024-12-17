b2fc699dc1765d45ef76e8046fdd82de6f0dccbb
Raherimalalanantenaina <168984775+Raherimalalanantenaina@users.noreply.github.com>
2024-12-17 16:24:57 +0100

    complet
    projet,base,docker

---
+++ fournisseurIdentiteApi/src/main/java/web/projet/fournisseurIdentite/controllers/UtilisateurController.java
@@ -0,0 +1,31 @@
+package web.projet.fournisseurIdentite.controllers;
+
+import web.projet.fournisseurIdentite.dtos.utilisateur.UtilisateurDTO;
+import web.projet.fournisseurIdentite.services.UtilisateurService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/utilisateurs")
+public class UtilisateurController {
+
+    private final UtilisateurService utilisateurService;
+
+    @Autowired
+    public UtilisateurController(UtilisateurService utilisateurService) {
+        this.utilisateurService = utilisateurService;
+    }
+
+    @PostMapping
+    public UtilisateurDTO create(@RequestBody UtilisateurDTO data) {
+        return utilisateurService.save(data);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UtilisateurDTO> update(@PathVariable Integer id, @RequestBody UtilisateurDTO data) {
+        return ResponseEntity.ok(utilisateurService.update(id, data));
+    }
+
+}
---
+++ fournisseurIdentiteApi/src/main/java/web/projet/fournisseurIdentite/services/UtilisateurService.java
@@ -0,0 +1,38 @@
+package web.projet.fournisseurIdentite.services;
+
+import web.projet.fournisseurIdentite.dtos.utilisateur.UtilisateurDTO;
+import web.projet.fournisseurIdentite.mappers.UtilisateurMapper;
+import web.projet.fournisseurIdentite.models.Utilisateur;
+import web.projet.fournisseurIdentite.repositories.UtilisateurRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class UtilisateurService {
+
+    private final UtilisateurRepository utilisateurRepository;
+    private final UtilisateurMapper utilisateurMapper;
+
+    @Autowired
+    public UtilisateurService(UtilisateurRepository utilisateurRepository,UtilisateurMapper utilisateurMapper) {
+        this.utilisateurRepository = utilisateurRepository;
+        this.utilisateurMapper = utilisateurMapper;
+    }
+
+    public UtilisateurDTO save(UtilisateurDTO data) {
+        Utilisateur utilisateur = utilisateurMapper.toUtilisateur(data);
+        return utilisateurMapper.toUtilisateurDTO(utilisateur);
+    }
+
+    public UtilisateurDTO update(Integer id, UtilisateurDTO data) {
+        Utilisateur existingUtilisateur = utilisateurRepository.findById(id).orElseThrow(() -> new RuntimeException("Utilisateur not found with id " + id));
+        Utilisateur updateUtilisateur = utilisateurMapper.toUtilisateur(data);
+        updateUtilisateur.setId(existingUtilisateur.getId());
+        Utilisateur savedUtilisateur = utilisateurRepository.save(updateUtilisateur);
+        return utilisateurMapper.toUtilisateurDTO(savedUtilisateur);
+    }
+
+}
