86f1cdaa06c4d0d8538ef52fa4fdacf748489056
ismaelBlaise <152376566+ismaelBlaise@users.noreply.github.com>
2024-12-18 13:20:03 +0300

    copy

--- fournisseurIdentiteApi/src/main/java/web/projet/fournisseurIdentite/dtos/utilisateur/UtilisateurDTO.java
+++ fournisseurIdentiteApi/src/main/java/web/projet/fournisseurIdentite/dtos/utilisateur/UtilisateurDTO.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 
 import lombok.Data;
 import web.projet.fournisseurIdentite.dtos.sexe.SexeDTO;
-import web.projet.fournisseurIdentite.models.Sexe;
 
 @Data
 public class UtilisateurDTO {
@@ -17,5 +16,5 @@ public class UtilisateurDTO {
     private String mot_de_passe;
     private Boolean etat = false;  
     private Integer nb_tentative = 0;
-    private SexeDt sexe;
+    private SexeDTO sexe;
 }
